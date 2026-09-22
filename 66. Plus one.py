a5ebc0fcd85cc7b59d1ef64446506e05e37f3a61
Jorden Coutinho <95267581+Jorden-Coutinho@users.noreply.github.com>
2026-09-22 21:44:27 +0530

    final solution using the list to integer method

--- 66. Plus one.py
+++ 66. Plus one.py
@@ -1,14 +1,8 @@
 class Solution:
     def plusOne(self, digits: list[int]) -> list[int]:
-        x=(len(digits)) - 1
-        if digits[x] == 9:
-            digits[x] = 1
-            digits.append(0)
-            y = x
-            while digits[y-1] == 9:
-                digits[y-1] = 1
-                digits[y] = 0
-                y-=1 
-        else:
-            digits[x] = digits[x] + 1
-        return digits
+        num = 0
+        for digit in digits:
+            num = num * 10 + digit
+        num += 1
+        x = [int(c) for c in str(num)]
+        return x
