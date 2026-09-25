af1b3621832c39068bf19ec305c53a8c75313932
Jorden Coutinho <95267581+Jorden-Coutinho@users.noreply.github.com>
2026-09-25 19:17:56 +0530

    Formatting and Labelling
    Refactor maximum69Number method to correct logic for returning modified number.

--- 1323. Max 69 Number.py
+++ 1323. Max 69 Number.py
@@ -1,22 +1,22 @@
+#method 1 - Integer Reversal and replacement
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        rev = 0
-        num1 = num
-        num2 = 0
+        rev = 0 
+        num1 = num 
         c = False
         while num1 > 0:
             d = num1 % 10
             rev = rev * 10 + d
             num1 //= 10
 
-
         while rev > 0:
             d = rev%10
             print(d)
-            if d == 6:
-                if c == False:
-                    d = 9
-                    c = True
-            num2 = num2*10+d
+            if d == 6 and c == False:
+                d = 9
+                c = True
+            num1 = num1*10+d
             rev //=10
-        return num2
+        return num1
+
+#method 2 - 
